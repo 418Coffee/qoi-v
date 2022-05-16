0a0bdea86b60cd5e92f982660e32a3a36cc6521e
418Coffee <matthijskes@gmail.com>
2022-05-16 18:50:23 +0200

    Update qoi.v

--- qoi.v
+++ qoi.v
@@ -11,21 +11,14 @@ struct Config {
 	colourspace u8
 }
 
-struct Qoi_rgba {
-mut:
- 	r u8
-	g u8
-	b u8
-	a u8
-}
-
 const (
     qoi_op_index =  0x00 /* 00xxxxxx */
     qoi_op_diff  =  0x40 /* 01xxxxxx */
-    qoi_op_luna  =  0x80 /* 10xxxxxx */
+    qoi_op_luma  =  0x80 /* 10xxxxxx */
     qoi_op_run   =  0xc0 /* 11xxxxxx */
     qoi_op_rgb   =  0xfe /* 11111110 */
     qoi_op_rgba  =  0xff /* 11111111 */
+	qoi_mask_2   =  0xc0 /* 11000000 */
 
 
 
@@ -37,6 +30,8 @@ const (
 	enough for anybody. */
 	qoi_pixels_max  = 400_000_000
 )
+
+type Operand = u8
 
 type Pixel = [4]u8
 
@@ -59,12 +54,10 @@ fn read_at_least(mut r io.Reader, mut buf []u8, min int) ? {
 	}
 }
 
-fn read_byte(mut r io.Reader) ?u8 {
-	mut buf := [u8(0)]
-	read_at_least(mut r, mut buf, 1)?
-	return buf[0]
-}
 
+fn color_hash(p Pixel) u8 {
+	return p[0] * 3 + p[1] * 5 + p[2] * 7 + p[3] * 11
+}
 
 
 pub fn decode_header(mut r io.Reader) ?Config {
@@ -85,7 +78,7 @@ pub fn decode_header(mut r io.Reader) ?Config {
 	}
 }
 
-pub fn decode(mut r io.Reader) ? {
+pub fn decode(mut r io.Reader) ?[]u8 {
 	cfg := decode_header(mut r)?
 	mut pixels := cfg.width * cfg.height
 	// https://twitter.com/v_language/status/1517099415143690240 :wow:
@@ -93,29 +86,57 @@ pub fn decode(mut r io.Reader) ? {
 		return error("invalid configuration")
 	}
 	println(cfg)
+	mut result := []u8{len: int(pixels*cfg.channels)}
 	mut bytes := []u8{len: int(pixels*cfg.channels)}
 	read_full(mut r, mut bytes)?
-	index := []Pixel{len: 64}
+	mut index := []Pixel{len: 64}
 	mut p := 14
 	mut run := 0
-	mut rgba := Qoi_rgba{}
-	for px_pos := 0; px_pos < bytes.len; px_pos += cfg.channels {
+	mut px := Pixel([4]u8{init: 0})
+	for px_pos := 0; px_pos < result.len; px_pos += cfg.channels {
 		if run > 0 {
 			run--
 		} else {
-			b1 := bytes[p++]
+			b1 := Operand(bytes[p++])
 			if b1 == qoi_op_rgb {
-				start := p
-				rgba.r = bytes[p++]
-				rgba.g = bytes[p++]
-				rgba.b = bytes[p++]
-				rgba.a = bytes[p++]
+				px[0] = bytes[p++]
+				px[1] = bytes[p++]
+				px[2] = bytes[p++]
+				px[3] = 0xff
+			} else if b1 == qoi_op_rgba {
+				px[0] = bytes[p++]
+				px[1] = bytes[p++]
+				px[2] = bytes[p++]
+				px[3] = bytes[p++]
+			} else if (b1 & qoi_mask_2) == qoi_op_index {
+				px = index[b1]
+			} else if (b1 & qoi_mask_2) == qoi_op_diff {
+				px[0] += ((b1 >> 4) & 0x03) - 2
+				px[1] += ((b1 >> 2) & 0x03) - 2
+				px[2] += ( b1       & 0x03) - 2
+			} else if (b1 & qoi_mask_2) == qoi_op_luma {
+				b2 := bytes[p++]
+				vg := (b1 & 0x3f) - 32
+				px[0] += vg - 8 + ((b2 >> 4) & 0x0f)
+				px[1] += vg
+				px[2] ++ vg - 8 +  (b2       & 0x0f)
+			} else if (b1 & qoi_mask_2) == qoi_op_run {
+				run = (b1 & 0x3f)
 			}
+			index[color_hash(px) % 64] = px
+		}
+		println(px)
+		result[px_pos+0] = px[0]
+		result[px_pos+1] = px[1]
+		result[px_pos+2] = px[2]
+		if cfg.channels == 4 {
+			result[px_pos+3] = px[3]
 		}
 	}
+	return result
 }
 
 fn main() {
-	mut f := os.open_file("./qoi_test_images/dice.qoi", "rb")?
-	decode(mut f)?
+	mut f := os.open_file("./qoi_test_images/testcard_rgba.qoi", "rb")?
+	res := decode(mut f)?
 }
